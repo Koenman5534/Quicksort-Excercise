0cde97a9c1ffa11803d12dac5da6cc950b8b0fc4
Unknown <sierdto@hotmail.com>
2018-11-26 12:52:26 +0100

    Version 1 of array Quicksort

--- Assignment-1/src/main/java/nl/hva/ict/ss/AdvancedSorts.java
+++ Assignment-1/src/main/java/nl/hva/ict/ss/AdvancedSorts.java
@@ -6,22 +6,47 @@ public class AdvancedSorts {
 
     /**
      * Implement quicksort using LinkedList only! Usage of anything but LinkedList will result in failing the assignment!
+     *
      * @param unsorted
      * @param <E>
      * @return
      */
     public static <E extends Comparable<E>> LinkedList<E> quickSort(LinkedList<E> unsorted) {
+
+        int pivot;
+        LinkedList<E> lowerSegmentList = new LinkedList<E>();
+        LinkedList<E> higherSegmentList = new LinkedList<E>();
+
+
+
+//        System.out.println(unsorted.toString());
+
+
         return unsorted;
+
     }
 
     /**
      * Implement quicksort using arrays only! Usage of anything but arrays will result in failing the assignment!
+     *
      * @param unsorted
      * @param <E>
      * @return
      */
     public static <E extends Comparable<E>> E[] quickSort(E[] unsorted) {
+        new QuickSort();
+
+        int high = unsorted.length;
+
+        QuickSort ob = new QuickSort();
+        ob.sort((Player[]) unsorted, 0, high);
+
+        System.out.println("sorted array");
+        QuickSort.printArray((Player[]) unsorted);
+
+//        System.out.println(pivot);
         return unsorted;
     }
+
 
 }
--- Assignment-1/src/test/java/nl/hva/ict/ss/ExtendedAdvancedSortingTest.java
+++ Assignment-1/src/test/java/nl/hva/ict/ss/ExtendedAdvancedSortingTest.java
@@ -1,5 +1,14 @@
 package nl.hva.ict.ss;
 
+import nl.hva.ict.ss.util.NameReader;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.security.SecureRandom;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Put your tests that show you implemented the code correctly in this class.
  * Any test placed at a different class will be ignored!
@@ -7,4 +16,11 @@ package nl.hva.ict.ss;
  */
 public class ExtendedAdvancedSortingTest extends AdvancedSortingTest {
 
+
+    @Test
+    public void quickSort() {
+
+        System.out.println("Eigen test. Leeg");
+
+    }
 }
