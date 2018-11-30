604b01c9f8601658669741dc5b844ba35530ec65
Koen van der Fluit <koenvanderfluit@hotmail.com>
2018-11-30 11:49:39 +0100

    Reformat and rename for the List section

--- Assignment-1/src/main/java/nl/hva/ict/ss/QuickSort.java
+++ Assignment-1/src/main/java/nl/hva/ict/ss/QuickSort.java
@@ -11,6 +11,7 @@ class QuickSort {
      * smaller (smaller than pivot) to left of
      * pivot and all greater elements to right
      * of pivot
+     *
      * @param arr
      * @param low
      * @param high
@@ -46,7 +47,7 @@ class QuickSort {
 
 
     /**
-     * @param arr Contains unsorted array
+     * @param arr  Contains unsorted array
      * @param low
      * @param high
      */
@@ -62,57 +63,60 @@ class QuickSort {
             sort(arr, pi + 1, high);
         }
     }
+    /* ----------------------------------------------------------------------------------------------------------
+       List implementation
+     */
 
     /**
      * Partition of the linkedlist (same as array, but with linkedlist)
-     * @param arr
+     *
+     * @param list
      * @param low
      * @param high
      * @return
      */
-    public int partition(LinkedList<Player> arr, int low, int high) {
-        Player playerPivot = arr.get(high);
+    private int partition(LinkedList<Player> list, int low, int high) {
+        Player playerPivot = list.get(high);
 
         // index of smaller element
         int i = (low - 1);
 
         for (int j = low; j < high; j++) {
 
-            if (arr.get(j) != null) {
-                if (arr.get(j).compareTo(playerPivot) >= 1) {
+            if (list.get(j) != null) {
+                if (list.get(j).compareTo(playerPivot) >= 1) {
                     i++;
 
                     // swap arr[i] and arr[j]
-                    Player temp = arr.get(i);
-                    arr.set(i, arr.get(j));
-                    arr.set(j, temp);
+                    Player temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
                 }
             }
         }
 
         // swap arr[i+1] and arr[high] (or pivot)
-        Player temp = arr.get(i + 1);
-        arr.set(i + 1, arr.get(high));
-        arr.set(high, temp);
+        Player temp = list.get(i + 1);
+        list.set(i + 1, list.get(high));
+        list.set(high, temp);
 
         return i + 1;
     }
 
-
     /**
-     * @param arr Contains unsorted linkedlist
+     * @param list Contains unsorted linkedlist
      * @param low
      * @param high
      */
-    void sort(LinkedList<Player> arr, int low, int high) {
+    void sort(LinkedList<Player> list, int low, int high) {
         if (low < high) {
 
-            int pi = partition(arr, low, high);
+            int pi = partition(list, low, high);
 
             // Recursively sort elements before
             // partition and after partition
-            sort(arr, low, pi - 1);
-            sort(arr, pi + 1, high);
+            sort(list, low, pi - 1);
+            sort(list, pi + 1, high);
         }
     }
 }
